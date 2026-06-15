0b1e39273e9f4586bcebfbfa03ce33a4827e9f28
Your Name <mrunalk1102@gmail.com>
2026-06-15 11:49:10 +0530

    #  wap to reverse

--- .py
+++ .py
@@ -1,3 +1,4 @@
+"""
 # wap to check number is prime or not using a function
 def prime(n):
     for i in range(2, int(n**0.5) + 1):
@@ -13,4 +14,13 @@ else:
     if prime(num):
         print(num, "is a prime number.")
     else:
-        print(num, "is not a prime number.")
+        print(num, "is not a prime number.")"""
+
+
+# wap to reverse any no using a function
+def reverse(n):
+    rev = 0
+    while n > 0:
+        rev = rev * 10 + n % 10
+        n //= 10
+    return rev
