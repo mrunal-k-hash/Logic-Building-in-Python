0edfb17ae93121c8d60e9ffa0f8be2a5889cc4b2
Your Name <mrunalk1102@gmail.com>
2026-06-16 01:11:19 +0530

    Add functions to calculate square root and area of triangle, and implement variable swapping

---
+++ logic_building.py
@@ -0,0 +1,45 @@
+# wap to calculate square root (exponentiation operator)
+
+"""num = float(input("enter a number: "))
+sr = num**0.5
+print("square root of given number is ", sr)"""
+
+# help of math module
+"""import math
+
+num = float(input("enter a number: "))
+sr = math.sqrt(num)
+print("square root of given number is ", sr)"""
+
+# wap to calculate area if triangle
+
+"""height = float(input("enter height of triangle: "))
+base = float(input("enter base of triangle:"))
+area = (1 / 2) * base * height
+print("area of triangle is ", area)"""
+
+
+# variable swapping using third variable
+
+"""x = float(input("enter a number: "))
+y = float(input("enter another number: "))
+print(x)
+print(y)
+z = x
+print("value of z afer swapping is ", z)
+
+x = y
+print("value of x after swapping is ", x)
+
+y = z
+print("value of y after swapping is", y)"""
+
+# variable swapping without using third variable
+
+x = float(input("enter a number: "))
+y = float(input("enter another number: "))
+print(x)
+print(y)
+x, y = y, x
+print("value of x ", x)
+print("value of y", y)
