904d40854b0a0d0acf7968501d4b7deccfc8979e
Your Name <mrunalk1102@gmail.com>
2026-06-15 11:44:51 +0530

    Implement prime number checking function and user input handling

--- .py
+++ .py
@@ -1,4 +1,16 @@
-num1 = 10
-num2 = 20
-sum = num1 + num2
-print("The sum of", num1, "and", num2, "is:", sum)
+# wap to check number is prime or not using a function
+def prime(n):
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+num = int(input("Enter a number: "))
+if num < 2:
+    print(num, "is not a prime number.")
+else:
+    if prime(num):
+        print(num, "is a prime number.")
+    else:
+        print(num, "is not a prime number.")
